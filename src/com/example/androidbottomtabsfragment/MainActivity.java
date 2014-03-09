14d010ca0e4238ebe391fad01861c2070c077e09
Ming <minhnq8@gmail.com>
2014-03-09 19:52:28 +0700

    Update MainActivity

--- src/com/example/androidbottomtabsfragment/MainActivity.java
+++ src/com/example/androidbottomtabsfragment/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends FragmentActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return true; 
 	}
 
 }
